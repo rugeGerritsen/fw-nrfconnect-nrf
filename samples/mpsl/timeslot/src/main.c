192df2a2e0aab7145c5852733491ede50e0ad968
Johan Stridkvist <johan.stridkvist@nordicsemi.no>
2020-06-10 14:02:34 +0200

    manifest: Update nrfxlib
    samples: Adapt mpsl timeslot example to the new timeslot API which allows more than one timeslot
    
    Signed-off-by: Johan Stridkvist <johan.stridkvist@nordicsemi.no>

--- samples/mpsl/timeslot/src/main.c
+++ samples/mpsl/timeslot/src/main.c
@@ -65,8 +65,11 @@ static void error(void)
 }
 
 static mpsl_timeslot_signal_return_param_t *mpsl_timeslot_callback(
+	uint8_t session_id,
 	uint32_t signal_type)
 {
+	(void) session_id; /* unused parameter */
+
 	mpsl_timeslot_signal_return_param_t *p_ret_val = NULL;
 
 	switch (signal_type) {
@@ -159,26 +162,28 @@ static void mpsl_nonpreemptible_thread(void)
 {
 	int err;
 	enum mpsl_timeslot_call api_call = 0;
+	int32_t session_id = -1; /* Initialize to invalid session id */
 
 	while (1) {
 		if (k_msgq_get(&mpsl_api_msgq, &api_call, K_FOREVER) == 0) {
 			switch (api_call) {
 			case OPEN_SESSION:
-				err = mpsl_timeslot_session_open(
+				session_id = mpsl_timeslot_session_open(
 					mpsl_timeslot_callback);
-				if (err) {
+				if (session_id < 0) {
 					error();
 				}
 				break;
 			case MAKE_REQUEST:
 				err = mpsl_timeslot_request(
+					session_id,
 					&timeslot_request_earliest);
 				if (err) {
 					error();
 				}
 				break;
 			case CLOSE_SESSION:
-				err = mpsl_timeslot_session_close();
+				err = mpsl_timeslot_session_close(session_id);
 				if (err) {
 					error();
 				}
@@ -218,6 +223,7 @@ static void console_print_thread(void)
 
 void main(void)
 {
+
 	int err = console_init();
 
 	if (err) {
